c9028a40545cc8814dc0d4ee54dacc7b5ca6cec8
Artem_2009 <artem.ruslanovich.yunusov@gmail.com>
2024-07-09 16:55:03 +0500

    updated 1 service test

--- src/test/java/com/example/demo/services/CarServiceTest.java
+++ src/test/java/com/example/demo/services/CarServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import com.example.demo.dtos.CarRequest;
+import com.example.demo.models.BookingRecord;
 import com.example.demo.models.Car;
 import com.example.demo.models.Person;
 import com.example.demo.models.enums.RulesBreaks;
@@ -17,8 +18,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -192,4 +196,51 @@ public class CarServiceTest {
         assertEquals(response, expected);
     }
 
+    @Test
+    public void TestGetCarsBookingRecordPositive() {
+        Person person = new Person(1L, "John", 1000, null, null, "smith");
+        UUID registrationNumber = UUID.randomUUID();
+        BookingRecord bookingRecord = new BookingRecord(1L, null, null, LocalDateTime.now(), LocalDateTime.now().plusHours(8L),registrationNumber, 1600);
+        Car car = new Car(1L, "u123ir", TypeOfCar.USUAL_CAR, person, Set.of(bookingRecord));
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getName()).thenReturn("smith");
+        when(personRepository.findByUsername("smith")).thenReturn(Optional.of(person));
+        when(carRepository.findCarByNumber("u123ir")).thenReturn(Optional.of(car));
+        ResponseEntity<Set<BookingRecord>> response = carService.getCarsBookingRecords("u123ir");
+        ResponseEntity<Set<BookingRecord>> expected = ResponseEntity.status(HttpStatus.OK).body(Set.of(bookingRecord));
+        assertEquals(response, expected);
+    }
+
+    @Test
+    public void TestGetCarsBookingRecordNegativeNoCar() {
+        Person person = new Person(1L, "John", 1000, null, null, "smith");
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getName()).thenReturn("smith");
+        when(personRepository.findByUsername("smith")).thenReturn(Optional.of(person));
+        when(carRepository.findCarByNumber("u123ir")).thenReturn(Optional.empty());
+        ResponseEntity<Set<BookingRecord>> response = carService.getCarsBookingRecords("u123ir");
+        ResponseEntity<Set<BookingRecord>> expected = ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        assertEquals(response, expected);
+    }
+
+    @Test
+    public void TestGetCarsBookingRecordNegativeWrongPerson() {
+        Person person = new Person(1L, "John", 1000, null, null, "smith");
+        UUID registrationNumber = UUID.randomUUID();
+        BookingRecord bookingRecord = new BookingRecord(1L, null, null, LocalDateTime.now(), LocalDateTime.now().plusHours(8L),registrationNumber, 1600);
+        Car car = new Car(1L, "u123ir", TypeOfCar.USUAL_CAR, new Person(2L, "Adam", 1000, null, null, "smith2"), Set.of(bookingRecord));
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+        when(authentication.getName()).thenReturn("smith");
+        when(personRepository.findByUsername("smith")).thenReturn(Optional.of(person));
+        when(carRepository.findCarByNumber("u123ir")).thenReturn(Optional.of(car));
+        ResponseEntity<Set<BookingRecord>> response = carService.getCarsBookingRecords("u123ir");
+        ResponseEntity<Set<BookingRecord>> expected = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        assertEquals(response, expected);
+    }
 }
